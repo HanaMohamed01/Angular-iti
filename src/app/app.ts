dbaca59326240f739de9a60cf5d171413566a680
Hana Mohamed <elnegeryh@gmail.com>
2025-07-26 15:28:39 +0300

    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./Components/navbar/navbar";
 import { Footer } from "./Components/footer/footer";
-import { Category } from "./Components/home/category/category";
-import { Product } from "./Components/home/product/product";
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Footer, Category, Product],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
